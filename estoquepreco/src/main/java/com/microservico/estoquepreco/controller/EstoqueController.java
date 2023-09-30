ecdc7d9fe8188c0c11caf1f498b81bcec4db0e5b
Maksuedson <maksuedson@hotmail.com>
2023-09-30 15:20:12 -0300

    Adicionado dependencia da librabbitmq para usar como commons e removido classes nao mais utilizadas

--- estoquepreco/src/main/java/com/microservico/estoquepreco/controller/EstoqueController.java
+++ estoquepreco/src/main/java/com/microservico/estoquepreco/controller/EstoqueController.java
@@ -1,5 +1,7 @@
 package com.microservico.estoquepreco.controller;
 
+import org.rabbitmq.contantes.RabbitMQConstantes;
+import org.rabbitmq.dto.EstoqueDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.microservico.estoquepreco.constantes.RabbitMQConstantes;
-import com.microservico.estoquepreco.dto.EstoqueDto;
 import com.microservico.estoquepreco.service.RabbitMQService;
 
 @RestController
