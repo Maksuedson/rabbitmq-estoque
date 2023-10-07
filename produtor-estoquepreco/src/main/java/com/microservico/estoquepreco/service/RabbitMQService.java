16cf33a522011509faa984ad76be6425e82b9967
Maksuedson <maksuedson@hotmail.com>
2023-10-07 20:50:23 -0300

    Adicionado ObjectMapper

--- produtor-estoquepreco/src/main/java/com/microservico/estoquepreco/service/RabbitMQService.java
+++ produtor-estoquepreco/src/main/java/com/microservico/estoquepreco/service/RabbitMQService.java
@@ -1,17 +1,29 @@
 package com.microservico.estoquepreco.service;
 
+import java.lang.runtime.ObjectMethods;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RabbitMQService {
 	
 	@Autowired
-	RabbitTemplate tabbitTemplate;
+	private RabbitTemplate tabbitTemplate;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	public void enviarMensagem(String nomeFila, Object mensagem) {
-		this.tabbitTemplate.convertAndSend(nomeFila, mensagem);
+		try {
+			String mensagemJson = this.objectMapper.writeValueAsString(mensagem);
+			this.tabbitTemplate.convertAndSend(nomeFila, mensagemJson);
+		} catch (Exception e) {
+			e.getCause();
+		}
 	}
 
 }
