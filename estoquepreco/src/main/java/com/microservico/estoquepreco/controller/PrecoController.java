89a4f2e0846eb8613bc4a7b73f72b6283f1f2792
Maksuedson <maksuedson@hotmail.com>
2023-10-07 19:55:57 -0300

    Corrigido controler e adicionado porta

--- estoquepreco/src/main/java/com/microservico/estoquepreco/controller/PrecoController.java
+++ estoquepreco/src/main/java/com/microservico/estoquepreco/controller/PrecoController.java
@@ -5,6 +5,7 @@ import org.rabbitmq.dto.PrecoDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservico.estoquepreco.service.RabbitMQService;
 
 @RestController
-@RequestMapping
+@RequestMapping ("preco")
 public class PrecoController {
 	
 	@Autowired
 	private RabbitMQService rabbitMQService;
 	
+	@PutMapping
 	private ResponseEntity alterarPreco(@RequestBody PrecoDto precoDto) {
 		System.out.println(precoDto.getCodigoProduto());
-		this.rabbitMQService.enviarMensagem(RabbitMQConstantes.FILA_ESTOQUE, precoDto);
+		this.rabbitMQService.enviarMensagem(RabbitMQConstantes.FILA_PRECO, precoDto);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
